3733128096fe17976cd81c1ec7b599c1f458d170
Marvin Castro <marvin.stefan.castro@gmail.com>
2021-07-30 02:06:24 +0800

    Removed a test thing

--- cogs/basics.py
+++ cogs/basics.py
@@ -15,10 +15,6 @@ class Basics(commands.Cog):
     async def do_say(self, ctx, *, msg: str):
         await ctx.send(msg)
         await ctx.message.delete()
-    
-    @commands.command(name='anitest')
-    async def do_anitest(self, ctx, *, title):
-        pass
 
     # Search anime on MAL
     @commands.command(name='anime', aliases=['animu', 'ani'])
